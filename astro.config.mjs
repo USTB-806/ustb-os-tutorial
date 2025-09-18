e76165035c8ce4a10c1a882313fcd6714b3f329f
wdlin233 <wdlin233@163.com>
2025-09-18 16:18:42 +0800

    Deploy to /ustb-os-tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import starlightThemeRapide from 'starlight-theme-rapide';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://USTB-806.github.io',
+	base: '/ustb-os-tutorial',
 	integrations: [
 		starlight({
 			plugins: [starlightThemeRapide()],
 			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/USTB-806/ustb-os-tutorial' }],
 			sidebar: [
 				{
 					label: 'Guides',
